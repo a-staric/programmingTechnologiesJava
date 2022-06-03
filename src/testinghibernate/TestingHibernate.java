b713087dbc7d2f61679e1b623e1346b71af48594
20515 <20515@1K122S1.dune.edu>
2022-06-03 08:32:31 +0200

    Initial commit, create project testing Hibernate/

---
+++ src/testinghibernate/TestingHibernate.java
@@ -0,0 +1,16 @@
+package testinghibernate;
+
+/**
+ *
+ * @author 20515
+ */
+public class TestingHibernate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("I'm here!");
+    }
+    
+}
